42bcb744709ba5a2ee125645f803e75f31846089
tkatiwa <tkatiwa@inoviopay.com>
2020-09-01 23:22:27 +0530

    Initial Commit

---
+++ inovio/sample/InovioRequestSample.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Inovio API request Parameters
+ */
+return [
+    'ccStatus' => [
+        "site_id"=> "22895",
+        "request_action"=> "CCSTATUS",
+        "request_ref_po_id"=> "56185639"
+    ]
+];
