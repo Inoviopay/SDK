663ee4894bdad01c37c6fbfd58991b293caf10bd
tkatiwa <tkatiwa@inoviopay.com>
2020-09-01 23:14:04 +0530

    Initial Commit

---
+++ inovio/sample/InovioRequestSample.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Inovio API request Parameters
+ */
+return [
+    'creditOnFail' => [
+        "site_id"=> "22895",
+        "request_action"=> "CCREVERSE",     
+        "request_ref_po_id"=> "56185639",
+        "credit_on_fail"=> 1
+    
+    ]
+];
